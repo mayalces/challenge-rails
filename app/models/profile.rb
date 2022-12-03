5fd512a22484cb0f0603c172f7493d0a741b49e3
Mayra Cespedes <mayalces@gmail.com>
2022-12-03 17:21:01 -0500

    Add alphanumeric and hyphens format to username

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  validates :username, format: { with: /\A[a-zA-Z0-9-]+\z/,
+    message: "only allows alphanumeric characters and hyphens (-)" }
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Profile, type: :model do
   describe 'validations' do
     subject { FactoryBot.build(:profile) }
     it { is_expected.to validate_uniqueness_of :username }
+    it { is_expected.to allow_value("foo-bar").for(:username) }
+    it { is_expected.to_not allow_value("f@@-bar").for(:username) }
   end
 end
